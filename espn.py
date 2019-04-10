20e2f930050e39d0d250e8a9db0ccb2bb500dda8
ktopz <kevintop@buffalo.edu>
2019-04-10 13:16:19 -0400

    completed first part of web scraper, including ESPN login

--- espn.py
+++ espn.py
@@ -1,6 +1,6 @@
 #Made by Kevin Topper
 from bs4 import BeautifulSoup
-import requests, json, time
+import requests, json, time, login, csv
 
 ############# returns list of schools that gave an offer ###############
 def offers(link):
@@ -17,53 +17,116 @@ def offers(link):
             retVal.append(offer)
     return retVal
 
+def picture(url):
+    z = requests.get(url)
+    soup = BeautifulSoup(z.text, features="lxml")
+    div = soup.find("div",{"class":"player-photo"})
+    ret = str(div).split('"')
+    return ret[3]
+
+#returns a string of a player's scouting report
+def scouting_report_post_2014(link):
+    return login.get_scouting_report_post_2014(link)
+def scouting_report_pre_2014(link):
+    return login.get_scouting_report_pre_2014(link)
+
+
 
 
 #pulling from espn
-source = requests.get('https://www.espn.com/college-sports/football/recruiting/playerrankings/_/view/rn300/sort/rank/class/2018').text
-soup = BeautifulSoup(source, 'html5lib')
-table = soup.find('div', class_='span-6', id = "my-players-table")
-tbody = table.find('tbody')
-
+year = 2006
+list = []
 
 #iterating over all rows of the ESPN table
-list = []
-for tr in tbody.find_all('tr'):
-    dict = {}
-    if(tr.find('div',class_='name') != None):
-        #############  ESPN URL  ###############
-        url_array = str(tr.find('div',class_='name').a).split('"')
-        url = url_array[1]
-        dict['url'] = url
-        #############  PLAYER NAME  ###############
-        name = tr.find('div',class_='name').strong.text
-        dict['name'] = name
+def script(year):
+    print("beginning year " + str(year))
+    ###########ESPN REQUEST##############
+    source = requests.get('https://www.espn.com/college-sports/football/recruiting/playerrankings/_/view/rn300/sort/rank/class/'+str(year)).text
+    soup = BeautifulSoup(source, 'html5lib')
+    table = soup.find('div', class_='span-6', id = "my-players-table")
+    tbody = table.find('tbody')
+    ##########################
+    progress = 1
+    for tr in tbody.find_all('tr'):
+        dict = {}
+        if(tr.find('div',class_='name') != None):
+            #############  ESPN URL  ###############
+            url_array = str(tr.find('div',class_='name').a).split('"')
+            url = url_array[1]
+            #dict['url'] = url
+            #############  PLAYER NAME  ###############
+            name = tr.find('div',class_='name').strong.text
+            dict['name'] = name
 
-        #############  HOMETOWN  ###############
-        td_array = tr.find_all('td')
-        hometown = str(td_array[3]).partition('<br/>')
-        hometown = hometown[0].split('>')
-        hometown = hometown[1]
-        dict['hometown'] = hometown
+            #############  HOMETOWN  ###############
+            td_array = tr.find_all('td')
+            hometown = str(td_array[3]).partition('<br/>')
+            hometown = hometown[0].split('>')
+            hometown = hometown[1]
+            dict['hometown'] = hometown
 
-        #############  HEIGHT  ###############
-        height = td_array[4].text
-        dict['height'] = height
+            #############  HEIGHT  ###############
+            height = td_array[4].text
+            dict['height'] = height
 
-        #############  WEIGHT  ###############
-        weight = td_array[5].text
-        dict['weight'] = weight
+            #############  WEIGHT  ###############
+            weight = td_array[5].text
+            dict['weight'] = weight
 
-        #############  OFFERS  ###############
-        offer_list = offers(url)
-        dict['offers'] = offer_list
+            #############  OFFERS  ###############
+            offer_list = offers(url)
+            dict['committed_to'] = offer_list[0]
+            dict['offers'] = offer_list
 
-        #############  debug stuff  ###############
-        #print(tr.prettify())
-        #print()
-        list.append(dict)
-        #break
-        print(dict)
-        time.sleep(2)
+            ########### PHOTO URL ###########
+            try:
+                dict['photo'] = picture(url)
+            except:
+                dict['photo'] = 'No Photo'
 
-print(list)
+            ######### SCOUTING REPORT ###########
+            insider_link = url.replace('www','insider')
+            temp = insider_link.split('/player')
+            new = temp[0] + '/player/evaluation' + temp[1]
+            #print(insider_link)
+            report = ""
+            try:
+                if(int(year) > 2013):
+                    report = scouting_report_post_2014(new)
+                else:
+                    report = scouting_report_pre_2014(new)
+            except:
+                report = ""
+            dict['report'] = report
+            ###############YEAR##############
+            dict['year'] = year
+
+            #############  debug stuff  ###############
+            #print(tr.prettify())
+            #print()
+            list.append(dict)
+            print(progress, end = ' ', flush = True)
+            progress+=1
+            time.sleep(.3)
+
+    #print(list)
+
+    '''
+    Now we must write our list to csv
+    '''
+
+while(year < 2019):
+    try:
+        script(year)
+    except:
+        print('year exception, restarting year ' + str(year))
+        script(year)
+    year+=1
+
+toCSV = list
+keys = toCSV[0].keys()
+with open('players.csv', 'w', encoding='utf-8') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(toCSV)
+print('csv is complete')
---
+++ login.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+
+#2014 is the cutoff
+
+headers={
+"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+"Accept-Encoding":"gzip, deflate",
+"Accept-Language":"en-US,en;q=0.9",
+"Cache-Control":"max-age=0",
+"Connection":"keep-alive",
+"Cookie":"s_vi=[CS]v1|2E30E37D8507FAC1-6000010B80003247[CE]; UNID=161b0119-eeeb-43c4-a5bd-18bf65c6a31b; ESPN-ONESITE.WEB-PROD.auth=disneyid; ESPN-ONESITE.WEB-PROD-ac=XUS; SWID={609FD710-E667-4B1A-AFDF-3F012F476107}; espnAuth={\"swid\":\"{609FD710-E667-4B1A-AFDF-3F012F476107}\"}; AMCVS_EE0201AC512D2BE80A490D4C%40AdobeOrg=1; dtcAuth=ESPN_PLUS; s_c6=1552324902648-New; s_sq=%5B%5BB%5D%5D; s_omni_lid=%5B%5BB%5D%5D; edition=espn-en-us; connectionspeed=full; edition-view=espn-en-us; country=us; broadbandAccess=espn3-true%2Cnetworks-false; DE2=\"dXNhO255O2J1ZmZhbG87YnJvYWRiYW5kOzU7NDs0OzUxNDs0Mi45NDE2OTstNzguODM3NDY7ODQwOzMzOzE0Njs2O3VzOw==\"; DS=\"dmVyaXpvbi5uZXQ7NzM3NDE1O21jaSBjb21tdW5pY2F0aW9ucyBzZXJ2aWNlcyBpbmMuIGRiYSB2ZXJpem9uIGJ1c2luZXNzOw==\"; _omnicwtest=1552952782951; _cb_ls=1; _cb=INLPmCbG6-5PVWap; _chartbeat2=.1546716755264.1552952784673.0000000000000001.Ded9SsDvN-TdDMwVqUBOi91hBXClzh.1; _cb_svref=null; UNID=161b0119-eeeb-43c4-a5bd-18bf65c6a31b; userZip=14221; AMCV_EE0201AC512D2BE80A490D4C%40AdobeOrg=-330454231%7CMCAID%7C2E30E37D8507FAC1-6000010B80003247%7CMCIDTS%7C17974%7CMCMID%7C90446614011983908062998598862853325162%7CMCAAMLH-1553557654%7C7%7CMCAAMB-1553557654%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1552960054s%7CNONE%7CvVersion%7C3.1.2; _omnicwtest=works; check=true; mbox=session#6957a3795a86445d9330af92625503bb#1552954715|PC#6957a3795a86445d9330af92625503bb.17_111#1616197655; block.check=false%7Cfalse; tveMVPDAuth=; tveAuth=; espn_s2=AECbUT4MJFMcl6uRccl3xkPIpkxK%2FvcelIMg6%2Brxxej2WBwlfPrk%2BUYUG0SpVLaO11AUqrEW47Z2SeAnwHS69KbOBrOlST8Dv6X9cx17JtMmh0PjmpewDH589eX3ipMlNVA2LSo13UYNpbeDrnrZjj8HKArFyMtTEQkXTNPHbrEo0CVfHFLnWmIcEND1Sbti2gko%2Bg05ExvXJHABhLPkRUK9IoOKoZrQvPmJtRZu2d5W008ctGcC6wFnAnx7COsw%2B9iDJeuVrM4J8N4UoU9yEcxR; SWID_NT=0; userZip=14221; visionBrowserSession=1552952855545; visionLocalSession={%22session_id_local%22:%221552952855536-2003121647567%22}; visionLocalVisitor={}; visionSession={%22timestamp%22:1552952855549}; visionVisitor={}; optimizelyEndUserId=oeu1552952855553r0.06638249259827878; __qca=P0-508644946-1552952856845; s_pers=%20s_c24%3D1552952864147%7C1647560864147%3B%20s_c24_s%3DFirst%2520Visit%7C1552954664147%3B%20s_gpv_pn%3Despndevcenter%253Adocumentation%253Aindex%7C1552954664161%3B; s_sess=%20s_cc%3Dtrue%3B%20s_omni_lid%3D%3B%20s_sq%3D%3B%20s_ppv%3D92%3B",
+"DNT":"1",
+"Host":"insider.espn.com",
+"Upgrade-Insecure-Requests":"1",
+"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36"
+}
+
+
+
+def get_scouting_report_post_2014(url):
+    report = ""
+    z = requests.get(url, headers=headers)
+    soup = BeautifulSoup(z.text, features="lxml")
+    trs = soup.find("div",{"id":"PlayerFiles"}).find("tbody").find_all("tr")
+
+    for tr in trs:
+        tds = tr.find_all("td")
+        if(tds[1].text!=''):
+            #print(tds[1].text)
+            report = tds[1].text + '\n\n'
+    return report
+
+
+def get_scouting_report_pre_2014(url):
+    report=""
+    z = requests.get(url, headers=headers)
+    soup = BeautifulSoup(z.text, features="lxml")
+    div = soup.find("div",{"class":"mod-content article"}).find_all("p")
+    for p in div:
+        if(len(p.text)>30):
+            report += p.text + '\n\n'
+    return report
+
+
+#get_scouting_report_pre_2014('http://insider.espn.com/college-sports/football/recruiting/player/evaluation/_/id/22510/myron-rolle')
